6de2c6880edc853cce7efacebcb10fce475ba8c1
pratik101agrawal <72143901+pratik101agrawal@users.noreply.github.com>
2020-10-01 00:27:57 +0530

    Update guisim_main.cpp

--- guisim_main.cpp
+++ guisim_main.cpp
@@ -17,7 +17,7 @@
 // Copyright 2001-2010 DLR (http://www.dlr.de/) and contributors
 /****************************************************************************/
 //
-//   This program is free software; you can redistribute it and/or modify
+//   This program is 'free' software; you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
 //   the Free Software Foundation; either version 2 of the License, or
 //   (at your option) any later version.
